a4455cc375a29570a32b81c8aabb0802313b4aa9
KaboneHajarah <Kabongehajarah@gmail.com>
2023-02-03 10:36:13 +0300

    studentRecords

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+var names= arrayOf("shaturah","rayan")
+    var age= arrayOf(23,20)
+    var phoneNumber= arrayOf(73338448,718762964)
+    val weight= arrayOf(66,68)
+    var citizenship= true
+    var student1= arrayOf(names[0],age[0],phoneNumber[0],weight[0], citizenship)
+    println(student1.contentToString())
+    var student2= arrayOf(names[1],age[1],phoneNumber[1],weight[1], !citizenship)
+        println(student2.contentToString())
+
+
+
+
+
+}
